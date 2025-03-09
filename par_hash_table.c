d7c9e22cf21165c87483cd5073a6329ed7c9afe1
kevinbach1 <144812541+kevinbach1@users.noreply.github.com>
2025-03-09 18:19:22 -0400

    Add files via upload

---
+++ par_hash_table.c
@@ -0,0 +1,22 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include "mythreads.h"
+#include <stdlib.h>
+#include <pthread.h>
+#include <dirent.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 6) {
+        fprintf(stderr, "usage: %s <N> <K> <directory> <output_file> <value>\n", argv[0]);
+        return 1;
+    }
+
+    DIR *dir = opendir(argv[4]);
+    if (dir == NULL) {
+        printf("opendir with %s failed\n", argv[4]);
+        return 1;
+    }
+    printf("Your code is running.\n");
+    return 0;
+}
---
+++ seq_hash_table.c
@@ -0,0 +1,14 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include "mythreads.h"
+#include <stdlib.h>
+#include <pthread.h>
+
+int main(int argc, char *argv[]) {
+    // if (argc != 2) {
+    //     fprintf(stderr, "usage: %s <directory>\n", argv[0]);
+    // }
+    printf("Your code is running.\n");
+    return 0;
+}
